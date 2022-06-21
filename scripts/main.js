2e6c735dc2c8c0a3088b8f98f35b215a4ae2adf9
Md. Shihab Shahriar <shihab.shahriar@enosisbd.com>
2022-06-21 06:45:37 +0000

    main.js edited online with Bitbucket

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,34 @@
+
+
 addPlaceForm.addEventListener("submit", handleRow)
+addPlaceForm.addEventListener("reset", handleReset)
 
 let touristPlacesIndex = {};
 let touristPlacesHTML = [];
+let thsort ="not-sorted";
+
+picture.onchange = () => {
+    const [file] = picture.files
+    if (file) {
+        previewImg.style.display = "block"
+        previewImg.src = URL.createObjectURL(file)
+    }
+}
+
 function handleRow(e) {
     e.preventDefault();
     const data = new FormData(addPlaceForm);
     addPlaceForm.reset()
+    previewImg.style.display = "none"
+    previewImg.src = "#"
+    thsort ="not-sorted";
     let table = document.getElementById("touristPlaceGrid").getElementsByTagName('tbody')[0];
     let id,trow;
-
-    if(data.get('row-id')!=0){
+    let pic = data.get('picture');
+    if(data.get('row-id')!=-1){
         id = data.get('row-id');
-        trow = table.rows.namedItem(`row-${id}`);
+        trow = table.rows.namedItem(`row-${id}`);       
+        pic = touristPlacesIndex[id]['picture'];
     }
     else{
         id = table.rows.length;
@@ -19,14 +36,18 @@ function handleRow(e) {
         trow = table.insertRow(id);
         trow.setAttribute("id",`row-${id}`);
     }
+    if(data.get('picture').name==""){
+        data.set('picture',pic);
+    }
+
     touristPlacesIndex[id] = Object.fromEntries(data);
     
     let objectURL = window.URL.createObjectURL(data.get("picture"));
     trow.innerHTML = `<td>${data.get("name")}</td>
                         <td>${data.get("address")}</td>
                         <td>${data.get("rating")}</td>
-                        <td><img src="${objectURL}" width="100" height="100"></td>
-                        <td>
+                        <td style="text-align:center;"><img src="${objectURL}" width="200" height="150"></td>
+                        <td id="table-buttons">
                             <button class="redButton" type="button" onclick="deleteRow(event)">Delete</button>
                             <button class="greenButton" type="button" onclick="updateRow(event)" value="${id}">Update</button>
                         </td>`
@@ -36,6 +57,11 @@ function handleRow(e) {
     document.getElementById("tableRating").className = "not-sorted";
     document.getElementById("form-container").style.display = "none";
     document.getElementById("table-container").style.display = "block";
+}
+
+function handleReset(e){
+    previewImg.style.display = "none"
+    previewImg.src = "#"
 }
 
 function deleteRow(event) {
@@ -53,6 +79,8 @@ function updateRow(event) {
     placeForm.elements["name"].value = formValues["name"];
     placeForm.elements["address"].value = formValues["address"];
     placeForm.elements["rating"].value = formValues["rating"];
+    previewImg.style.display = "block";
+    previewImg.src = URL.createObjectURL(formValues["picture"]);
 
     document.getElementById("table-container").style.display = "none";
     document.getElementById("form-container").style.display = "block";
@@ -69,29 +97,38 @@ backToTable.onclick = function () {
     document.getElementById("table-container").style.display = "block";
 }
 
+
+
 touristPlaceGrid.onclick = function(e) {
     if (e.target.tagName != 'TH' || e.target.getAttribute("id")!= "tableRating") return;
-    let thsort = e.target.getAttribute("classname");
-    let thIndex = e.target.cellIndex;
     let sortedRows = Array.from(touristPlaceGrid.tBodies[0].rows);
 
     if(thsort=="not-sorted"){
-        sortedRows = sortedRows.sort((rowA, rowB) => rowA.cells[thIndex].innerHTML.localeCompare(rowB.cells[thIndex].innerHTML))
-        e.target.setAttribute("classname","sorted-asc");
+        sortedRows = sortedRows.sort((rowA, rowB) => rowA.cells[2].innerHTML.localeCompare(rowB.cells[2].innerHTML));
+        thsort = "sorted-asc";
     }
-    if(thsort=="sorted-asc"){
+    else if(thsort=="sorted-asc"){
         sortedRows.reverse();
-        e.target.setAttribute("classname","sorted-dsc");
+        thsort = "sorted-dsc";
     }
-    if(thsort=="sorted-dsc"){
-        sortedRows = touristPlacesHTML
-        e.target.setAttribute("classname","not-sorted");
+    else if(thsort=="sorted-dsc"){
+        sortedRows = touristPlacesHTML;
+        thsort = "not-sorted";
     }
+
     touristPlaceGrid.tBodies[0].append(...sortedRows);
 }
 
 function autocompleteMatch(input) {
-    touristPlaceGrid.tBodies[0].append(...touristPlacesHTML);
+    sortedRows = touristPlacesHTML;
+    if(thsort=="sorted-asc"){
+        sortedRows = touristPlacesHTML.sort((rowA, rowB) => rowA.cells[2].innerHTML.localeCompare(rowB.cells[2].innerHTML));
+    }
+    else if(thsort=="sorted-dsc"){
+        sortedRows = touristPlacesHTML.sort((rowA, rowB) => rowA.cells[2].innerHTML.localeCompare(rowB.cells[2].innerHTML));
+        sortedRows.reverse();
+    }
+    touristPlaceGrid.tBodies[0].append(...sortedRows);
     if (input == '') {
       return [];
     }
